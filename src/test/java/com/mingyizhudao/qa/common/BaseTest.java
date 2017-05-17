d4a0f14a413563b426318a3a289cd71415328ba5
ttshmily <ttshmilyfc@163.com>
2017-05-17 14:22:37 +0800

    add bda host in config and BastTest

--- src/test/java/com/mingyizhudao/qa/common/BaseTest.java
+++ src/test/java/com/mingyizhudao/qa/common/BaseTest.java
@@ -28,6 +28,7 @@ public class BaseTest {
     public static String host_crm = "";
     public static String host_login = "";
     public static String host_kb = "";
+    public static String host_bda = "";
     public static String crm_token = "";
 
     public static String mainMobile = "";
@@ -69,16 +70,19 @@ public class BaseTest {
         {
             // 初始化配置文件中的变量
             protocol = prop.getProperty("protocol");
+
             host_doc = prop.getProperty("host_doc");
             host_crm = prop.getProperty("host_crm");
             host_login = prop.getProperty("host_login");
             host_kb = prop.getProperty("host_kb");
+            host_bda = prop.getProperty("host_bda");
             crm_token = prop.getProperty("crm_token");
 
             host_doc = protocol.concat("://").concat(host_doc);
             host_crm = protocol.concat("://").concat(host_crm);
             host_login = protocol.concat("://").concat(host_login);
             host_kb = protocol.concat("://").concat(host_kb);
+            host_bda = protocol.concat("://").concat(host_bda);
         }
     }
 
---
+++ src/test/java/com/mingyizhudao/qa/testcase/bdassistant/SearchHospital.java
@@ -0,0 +1,7 @@
+package com.mingyizhudao.qa.testcase.bdassistant;
+
+/**
+ * Created by ttshmily on 17/5/2017.
+ */
+public class SearchHospital {
+}
