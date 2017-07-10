2ee131622d6a1c83bbcd952b36a4306f47e86bf4
ttshmily <ttshmilyfc@163.com>
2017-07-10 18:37:24 +0800

    比例分成接口用例

---
+++ src/test/java/com/mingyizhudao/qa/testcase/crm/Order_ThreewayCall_v2.java
@@ -0,0 +1,7 @@
+package com.mingyizhudao.qa.testcase.crm;
+
+/**
+ * Created by dayi on 2017/7/10.
+ */
+public class Order_ThreewayCall_v2 {
+}
---
+++ src/test/java/com/mingyizhudao/qa/testcase/doctor/SurgeryCategory.java
@@ -0,0 +1,7 @@
+package com.mingyizhudao.qa.testcase.doctor;
+
+/**
+ * Created by dayi on 2017/7/7.
+ */
+public class SurgeryCategory {
+}
