c70ced7cc205140ef2fde5413b4679da94234c8d
tianjing1 <422391950@qq.com>
2017-09-01 14:34:07 +0800

    src/test/java/com/mingyizhudao/qa/functiontest/doctor/CreateOrder.java

--- src/test/java/com/mingyizhudao/qa/functiontest/doctor/CreateOrder.java
+++ src/test/java/com/mingyizhudao/qa/functiontest/doctor/CreateOrder.java
@@ -8,8 +8,6 @@ import com.mingyizhudao.qa.utilities.Generator;
 import com.mingyizhudao.qa.utilities.Helper;
 import com.mingyizhudao.qa.utilities.HttpRequest;
 import net.sf.json.JSONObject;
-import net.sf.json.JSONSerializer;
-import net.sf.json.JsonConfig;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.HashMap;
@@ -31,6 +29,7 @@ public class CreateOrder extends BaseTest {
     public static String uri = "/api/createorder";
 
     public static String s_CreateOrder(String token){
+
         SurgeryOrder su = new SurgeryOrder("order");
         String res = HttpRequest.s_SendPost(host_doc + uri, su.transform(), token);
         return JSONObject.fromObject(res).getJSONObject("data").getString("order_number");
@@ -39,10 +38,18 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_01_创建订单_信息齐备_已认证用户() {
 
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(su).toString(), mainToken);
+        res = HttpRequest.s_SendPost(host_doc + uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000", "创建订单失败");
         String orderId = s_ParseJson(data, "order_number");
@@ -76,11 +83,11 @@ public class CreateOrder extends BaseTest {
         //Assert.assertEquals(s_ParseJson(data,"medical_record_pictures(0):key"), "2017/05/04/1265834e-97d8-44a0-95e7-047c7facaee8/IMG_20170429_102737.jpg");
         //Assert.assertEquals(s_ParseJson(data,"medical_record_pictures(1):key"), "2017/05/04/1315bbe0-2836-4776-8216-ec55044f32dd/IMG_20161013_172442.jpg");
         Assert.assertNotNull(s_ParseJson(data, "agent_contact_id"));
-        Assert.assertEquals(s_ParseJson(data, "agent_referrer_id"), "SH0133"); //常州市武进人民医院, 常州，区域服务人员 - 方超 - SH0133
-        Assert.assertEquals(s_ParseJson(data, "agent_referrer_name"), "方超"); //常州市武进人民医院, 常州，区域服务人员 - 方超 - SH0133
+        Assert.assertEquals(s_ParseJson(data, "agent_referrer_id"), "SH0133"); //北京大学口腔医院, 北京，区域服务人员 - 方超 - SH0133
+        Assert.assertEquals(s_ParseJson(data, "agent_referrer_name"), "方超"); //北京大学口腔医院, 北京，区域服务人员 - 方超 - SH0133
         Assert.assertNotNull(s_ParseJson(data, "agent_city_id"));
         Assert.assertNotNull(s_ParseJson(data, "agent_city_name"));
-        Assert.assertEquals(s_ParseJson(data, "agent_hospital"), "常州市武进人民医院");
+        Assert.assertEquals(s_ParseJson(data, "agent_hospital"), "北京大学口腔医院");
         Assert.assertNotNull(s_ParseJson(data, "agent_referrer_group_id"));
     }
 
@@ -89,7 +96,7 @@ public class CreateOrder extends BaseTest {
 
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(su).toString(), "");
+        res = HttpRequest.s_SendPost(host_doc + uri, su.transform(), "");
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000");
         Assert.assertEquals(code, "2210304");
@@ -98,23 +105,29 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_03_创建订单_缺少患者姓名不可以创建() {
 
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
         su.getOrder().setPatient_name("");
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(),mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000");
 
         su.getOrder().setPatient_name("abcdefghijklmnopqrstuvwxyz一二三四五六七八九十甲乙丙地子卯寅丑");
-        res = HttpRequest.s_SendPost(host_doc+uri,JSONObject.fromObject(su).toString(),mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000");
 
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"patient_name"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        su.getOrder().setPatient_name(null);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000");
     }
@@ -122,18 +135,24 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_04_创建订单_缺少患者性别或性别不正确不可以创建() {
 
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
         su.getOrder().setPatient_gender(3);
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(), mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000");
 
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"patient_gender"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        su.getOrder().setPatient_gender(null);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000", "性别为3不应该能创建病历");
 
@@ -142,12 +161,19 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_05_创建订单_缺少患者年龄不可以创建() {
 
-        String res = "";
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
 
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"patient_age"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        String res = "";
+        SurgeryOrder su = new SurgeryOrder("order");
+
+        su.getOrder().setPatient_age(null);
+        res = HttpRequest.s_SendPost(host_doc + uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000");
 
@@ -156,18 +182,24 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_06_创建订单_缺少患者手机不可以创建() {
 
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
         su.getOrder().setPatient_phone("");
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(), mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code,"1000000");// PD要求可以创建。。。
 
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"patient_phone"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        su.getOrder().setPatient_phone(null);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000"); // PD要求可以创建。。。
     }
@@ -175,18 +207,24 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_07_创建订单_缺少主诉疾病不可以创建() {
 
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
         su.getOrder().setMajor_disease_id("");
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(),mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code,"1000000");
 
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"major_disease_id"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        su.getOrder().setMajor_disease_id(null);
+        res = HttpRequest.s_SendPost(host_doc + uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertNotEquals(code, "1000000");
 
@@ -195,18 +233,24 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_08_创建订单_缺少次诉疾病可以创建() {
 
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
         su.getOrder().setMinor_disease_id("");
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(), mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code,"1000000");
 
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"minor_disease_id"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        su.getOrder().setMinor_disease_id(null);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000");
     }
@@ -252,62 +296,78 @@ public class CreateOrder extends BaseTest {
     @Test
     public void test_11_创建订单_信息齐备_期望手术医院不传() {
 
-        String res = "";
+        String userToken = "";
+        String userDoctorHospitalId = "";
+        String userDoctorHospitalName = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+        userDoctorHospitalId = mainDoctorInfo.get("hospitalId");
+        userDoctorHospitalName = Generator.hospitalName(userDoctorHospitalId);
 
+        String res = "";
         SurgeryOrder su = new SurgeryOrder("order");
 
         logger.info("不传入期望手术医院的ID。。。");
         su.getOrder().setExpected_surgery_hospital_id("");
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(),mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000", "创建订单失败");
         String orderId = Helper.s_ParseJson(data, "order_number");
         Assert.assertNotEquals(orderId, "", "返回的订单ID格式有误");
         logger.info("查看刚刚创建的订单详情");
-        res = GetOrderDetail_V1.s_MyInitiateOrder(mainToken, orderId);
+        res = GetOrderDetail_V1.s_MyInitiateOrder(userToken, orderId);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000");
-        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_id"), mainDoctorHospitalId);
-        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_name"), mainDoctorHospitalName);
+        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_id"), userDoctorHospitalId);
+        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_name"), userDoctorHospitalName);
 
         logger.info("传入期望手术医院的ID=0。。。");
         su.getOrder().setExpected_surgery_hospital_id("0");
-        res = HttpRequest.s_SendPost(host_doc+uri, JSONObject.fromObject(su).toString(), mainToken);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000", "创建订单失败");
         orderId = Helper.s_ParseJson(data, "order_number");
         Assert.assertNotEquals(orderId, "", "返回的订单ID格式有误");
         logger.info("查看刚刚创建的订单详情");
-        res = GetOrderDetail_V1.s_MyInitiateOrder(mainToken, orderId);
+        res = GetOrderDetail_V1.s_MyInitiateOrder(userToken, orderId);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000");
         Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_id"), "0");
         Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_name"), "待回访");
 
         logger.info("不传入期望手术医院的key。。。");
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"expected_surgery_hospital_id"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        su.getOrder().setExpected_surgery_hospital_id(null);
+        res = HttpRequest.s_SendPost(host_doc+uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000", "创建订单失败");
         orderId = Helper.s_ParseJson(data, "order_number");
         Assert.assertNotEquals(orderId, "", "返回的订单ID格式有误");
         logger.info("查看刚刚创建的订单详情");
-        res = GetOrderDetail_V1.s_MyInitiateOrder(mainToken, orderId);
+        res = GetOrderDetail_V1.s_MyInitiateOrder(userToken, orderId);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000");
-        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_id"), mainDoctorHospitalId);
-        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_name"), mainDoctorHospitalName);
+        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_id"), userDoctorHospitalId);
+        Assert.assertEquals(Helper.s_ParseJson(data,"order:expected_surgery_hospital_name"), userDoctorHospitalName);
     }
 
     @Test
     public void test_12_创建订单_病例图片作为非必填字段() {
+
+        String userToken = "";
+        HashMap<String,String> mainDoctorInfo = s_CreateSyncedDoctor(mainUser);
+        if(mainDoctorInfo == null) {
+            logger.error("创建注册专家失败，退出执行");
+            System.exit(10000);
+        }
+        userToken = mainDoctorInfo.get("token");
+
         String res = "";
-        JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.setExcludes(new String[]{"medical_record_pictures"});
-        SurgeryOrder su1 = new SurgeryOrder("order");
-        res = HttpRequest.s_SendPost(host_doc + uri, JSONObject.fromObject(JSONSerializer.toJSON(su1, jsonConfig)).toString(), mainToken);
+        SurgeryOrder su = new SurgeryOrder("order");
+        res = HttpRequest.s_SendPost(host_doc + uri, su.transform(), userToken);
         s_CheckResponse(res);
         Assert.assertEquals(code, "1000000");
     }
